6d876ce06222a6d2459a8fd8145f2ed7165c50b4
jaeho310 <chlwogh1993@gmail.com>
2022-12-07 23:46:47 +0900

    commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"graphql-sample/generated"
+	"graphql-sample/resolver"
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+func main() {
+	port := "8080"
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(
+		generated.Config{Resolvers: &resolver.Resolver{}}))
+	http.Handle("/", playground.Handler("Graphql playground", "/api/graphql"))
+	http.Handle("/api/graphql", srv)
+	log.Printf("http://localhost:%s for Graphql platground \n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
